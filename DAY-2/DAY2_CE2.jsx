42da2cb6f15823e3b643da887c3717c56865d20d
keerthika09-R <727723eucs104@skcet.ac.in>
2024-08-06 07:25:49 +0530

    Add files via upload

---
+++ DAY-2/DAY2_CE2.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Message=()=> {
+    return (
+        <div>
+            <h1 style={{color:'green'}}>Inline Style in JSX Example.</h1>
+            <div style={{background:'lightblue',color:'darkblue',padding:'10px',border:'1px solid blue.',borderRadius:'5px'}}>
+            <p style={{color:'darkblue',fontSize:'16px'}}>This is a paragraph with inline styles applied.</p></div>
+        </div>
+    );
+};
+export default Message;
