e1b5c93a1a7be67714e640b80a5162ae996795ed
keerthika09-R <727723eucs104@skcet.ac.in>
2024-08-07 23:31:18 +0530

    Add files via upload

---
+++ DAY-2/DAY2_CY.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+const Pree=() =>{
+  return(
+    <div 
+        style={{display:'flex',
+        flexDirection:'column',
+        alignItems:'center',
+        justifyContent:'center',
+        backgroundColor:'lightskyblue'}}>
+      <h1 >Smile Component</h1>
+      <p >It is a functional component</p>
+      <img src="https://cdn.pixabay.com/photo/2020/12/27/20/24/smile-5865208_640.png">
+      </img>
+    </div>
+    
+  );
+};
+export default Pree;
