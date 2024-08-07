3376867513e5ebbf519236acdc2cc47808a56630
keerthika09-R <727723eucs104@skcet.ac.in>
2024-08-07 23:35:14 +0530

    Add files via upload

---
+++ DAY-2/DAY2_DC.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const Car=() =>{
+  return(
+    <div style={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center',backgroundColor:'lightskyblue'}}>
+      <h1 >Car and Location Info</h1>
+      <p style={{textAlign:'center',backgroundColor:'lightgray',padding:'100px',fontFamily:'monospace',fontSize:'30px'}} >Porsche Model <br></br>
+      <img src="https://imgd-ct.aeplcdn.com/1056x660/n/xsoupsa_1476828.jpg?q=80" style={{ width:'500px',height:'300px'}}></img>
+      </p>
+    </div>
+  );
+};
+export default Car;
